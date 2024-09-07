4f3883d372199cdaef77eecd1a37e4979a07f996
Bernhard Bablok <bablokb@gmx.de>
2024-09-07 19:31:30 +0200

    added template-file for secrets.py

---
+++ examples/sec_template.py
@@ -0,0 +1,25 @@
+# -------------------------------------------------------------------------
+# Template secrets.py file. Copy to secrets.py and configure as needed.
+#
+# Author: Bernhard Bablok
+# License: MIT
+#
+# Website: https://github.com/bablokb/circuitpython-esp32at
+#
+# -------------------------------------------------------------------------
+
+secrets = {
+  'ssid' : 'my_ssid',
+  'password' : 'my_password',
+  'time_api_url': 'http://worldtimeapi.org/api/ip',
+  'tx_power': 15,
+  'hostname': 'test',
+  'ipv4_address': '192.168.41.10',
+  'ipv4_subnet': '255.255.255.0',
+  'ipv4_gateway': '192.168.41.10',
+  'dns_server': ['4.4.4.4', '208.67.222.222', '208.67.220.220'],
+  'udp_host': '192.168.41.20',
+  'udp_port': 6500,
+  'METEO_LATITUDE': 53.1234,
+  'METEO_LONGITUDE': 14.3210
+  }
