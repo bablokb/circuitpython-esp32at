c75806d662fe75aff2d35df9d894325915e7894c
Bernhard Bablok <bablokb@gmx.de>
2024-09-09 15:24:52 +0200

    added non-SSL HTTP-get example

---
+++ examples/query_timeapi.py
@@ -0,0 +1,106 @@
+# -------------------------------------------------------------------------
+# query_timeapi.py: simple, non-ssl get requests from worldtimeapi.org.
+#
+# Note that worldtimeapi.org uses rate-limiting, so don't flood them.
+#
+# This program needs the adafruit_requests and adafruit_connection_manager
+# modules.
+#
+# You need to create a secrets.py file (have a look at sec_template.py).
+#
+# Author: Bernhard Bablok
+# License: MIT
+#
+# Website: https://github.com/bablokb/circuitpython-esp32at
+#
+# -------------------------------------------------------------------------
+
+import time
+import board
+import busio
+import wifi
+import socketpool
+import adafruit_requests
+
+ITERATIONS = 10
+INTERVAL = 100
+DEBUG  = False
+
+if board.board_id == "raspberry_pi_pico":
+  PIN_TX  = board.GP0
+  PIN_RX  = board.GP1
+  PIN_RST = None
+elif board.board_id == "challenger_rp2350_wifi6_ble5":
+  PIN_TX  = board.ESP_RXD
+  PIN_RX  = board.ESP_TXD
+  PIN_RST = board.ESP_RESET
+else:
+  print(f"no pin-defs for board {board.board_id}")
+
+# Get wifi details and more from a secrets.py file
+try:
+  from secrets import secrets
+except ImportError:
+  print("WiFi secrets are kept in secrets.py, please add them there!")
+  raise
+
+# --- test if we don't have native wifi   ------------------------------------
+
+if hasattr(wifi,"at_version"):
+  uart = busio.UART(PIN_TX, PIN_RX, baudrate=115200, receiver_buffer_size=2048)
+  wifi.init(uart,debug=DEBUG,reset_pin=PIN_RST)
+  if not wifi.at_version:
+    raise RuntimeError("could not setup co-processor")
+  print(wifi.at_version)
+  wifi.radio.start_station()
+
+# --- set TX power if requested   --------------------------------------------
+
+print(f"TX power: {wifi.radio.tx_power}")
+if 'tx_power' in secrets:
+  print(f"changing TX power to {secrets['tx_power']}")
+  wifi.radio.tx_power = secrets['tx_power']
+  print(f"TX power: {wifi.radio.tx_power}")
+
+# --- connect to AP   --------------------------------------------------------
+
+print(f"connecting to AP {secrets['ssid']} ...")
+if 'timeout' in secrets:
+  timeout = secrets['timeout']
+else:
+  timeout = 5
+if 'retries' in secrets:
+  retries = secrets['retries']
+else:
+  retries = 3
+
+state = wifi.radio.connected
+print(f"  connected: {state}")
+if not state:
+  # ESP32xx defaults to automatic reconnection to old AP. It will
+  # disconnect first if already connected to an AP.
+  #
+  # Note: you can pass retries to wifi.radio.connect, but that is not portable
+  for _ in range(retries):
+    try:
+      wifi.radio.connect(secrets['ssid'],
+                         secrets['password'],
+                         timeout = timeout
+                         )
+      break
+    except ConnectionError as ex:
+      print(f"{ex}")
+  print(f"  connected: {wifi.radio.connected}")
+
+# --- query time-information from worldtimeapi.org   -------------------------
+
+url = secrets['time_api_url']
+
+pool = socketpool.SocketPool(wifi.radio)
+requests = adafruit_requests.Session(pool)
+
+for i in range(ITERATIONS):
+  response = requests.get(url)
+  print(f"{response.json()['datetime']}")
+  time.sleep(INTERVAL)
+response.close()
