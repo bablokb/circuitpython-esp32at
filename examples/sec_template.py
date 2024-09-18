b524655c65629e27de623240ab16dcc5bbc1245d
Bernhard Bablok <bablokb@gmx.de>
2024-09-18 15:18:07 +0200

    grouped settings and added comments

--- examples/sec_template.py
+++ examples/sec_template.py
@@ -9,17 +9,35 @@
 # -------------------------------------------------------------------------
 
 secrets = {
+  # WLAN credentials
   'ssid' : 'my_ssid',
   'password' : 'my_password',
-  'time_api_url': 'http://worldtimeapi.org/api/ip',
-  'tx_power': 15,
+
+  # transmit power
+  #'tx_power': 15,
+
+  # optional parameters for wifi.init()
+  #'ipv4_dns_defaults': ['4.4.4.4', '208.67.222.222', '208.67.220.220'],
+  #'country_settings': [False,'xx',1,13],
+  #'at_timeout': 1,
+  #'at_retries': 1,
+  #'reset': False,
+  #'persist_settings': True,
+  #'reconn_interval': 1,
+  #'multi_connection': False,
+  #'baudrate': 460800,
+
+  # settings for test_sta.py
   'hostname': 'test',
   'ipv4_address': '192.168.41.10',
   'ipv4_subnet': '255.255.255.0',
   'ipv4_gateway': '192.168.41.10',
   'dns_server': ['4.4.4.4', '208.67.222.222', '208.67.220.220'],
-  'udp_host': '192.168.41.20',
-  'udp_port': 6500,
-  'METEO_LATITUDE': 53.1234,
-  'METEO_LONGITUDE': 14.3210
+
+  # settings for other test programs
+  'time_api_url': 'http://worldtimeapi.org/api/ip', # query_timeapi.py
+  'udp_host': '192.168.41.20',                      # udp_logger.py
+  'udp_port': 6500,                                 # udp_logger.py
+  'METEO_LATITUDE': 53.1234,                        # query_openmeteo.py
+  'METEO_LONGITUDE': 14.3210                        # query_openmeteo.py
   }
