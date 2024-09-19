3e06bd07d8786b3af91438a5160cb4ea674bba69
Bernhard Bablok <bablokb@gmx.de>
2024-09-19 15:27:06 +0200

    wifi.init(): return result of transport.init()

--- wifi/__init__.py
+++ wifi/__init__.py
@@ -38,13 +38,14 @@ def init(uart: busio.UART,
          ipv4_dns_defaults: Optional[Sequence[str]] = [],
          country_settings: Optional = [0,None,None,None],
          **kwargs,
-         ) -> None:
+         ) -> Bool:
   """ initialize wifi-hardware (i.e. the co-processor """
   global at_version, radio # pylint: disable=invalid-name,global-statement
 
-  transport.init(uart,**kwargs)
-  at_version = transport.at_version # pylint: disable=invalid-name
-  if at_version:
+  rc = transport.init(uart,**kwargs)
+  if rc:
+    at_version = transport.at_version # pylint: disable=invalid-name
     if len(ipv4_dns_defaults):
       radio.ipv4_dns_defaults = ipv4_dns_defaults
     radio.country_settings = country_settings
+  return rc
